076b845a378c854a3f43abad2bb5fa949639b485
Faizanlarvel <alifaizansamoon110@gmail.com>
2025-04-11 10:05:20 -0700

    Add files via upload

---
+++ CartController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
+
+class CartController extends Controller
+{
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+
+        // Session ke andar cart set karna
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+        } else {
+            $cart[$id] = [
+                "name" => $product->name,
+                "price" => $product->price,
+                "image" => $product->image,
+                "quantity" => 1
+            ];
+        }
+
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product added to cart!');
+    }
+}
